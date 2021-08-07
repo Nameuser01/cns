88d0ce98694b75c0c2e9da92cb220d63f013df8c
Nameuser01 <antoine.rimbault@hotmail.com>
2021-08-07 03:09:04 +0200

    J'ai trouvé comment fix le problème du nombre de commentaires sur la page note.php

--- note_refresh.php
+++ note_refresh.php
@@ -1,67 +1,4 @@
 <?php
-	// try{
-	// 	$bdd = new PDO('mysql:host=localhost;dbname=mywiki;charset=utf8', 'root', '');
-	// }
-	// catch(Exeption $e){
-	// 	die('Erreur : ' . $e->getMessage());
-	// }
-	// $req_com = $bdd->query('SELECT COUNT(*) AS nbr_coms from note_bis');
-	// $data = $req_com->fetch();
-	// $nbr_coms = htmlspecialchars($data['nbr_coms']);
-
-	// $req = $bdd->query('SELECT secondaryID FROM note_bis');
-
-	
-	// $row = $req->fetch();
-	// 	for ($new_id = 1; $new_id < 7; $new_id++)
-	// 	{
-	// 		$donnees->execute(array($new_id, $row['secondaryID']));
-	// }
-	// $new_id = 0;
-	// for ($new_id = 1; $new_id < 7; $new_id++){
-	// 	$row = $req->fetch();
-	// 	$donnees = $bdd->prepare("UPDATE note_bis SET secondaryID = $new_id");
-	// 	$donnees->execute(array($new_id, $row['secondaryID']));
-	// 	// $new_id++;
-	// }
-
-	// $req = $bdd->query('SELECT secondaryID FROM note_bis');
-	// $new_id = 0;
-	// while($data = $req->fetch())
-	// {
-	// 	$req_com = $bdd->prepare("UPDATE note_bis SET secondaryID = 2");
-	// 	$donnes->execute(array($new_id, $data['secondaryID']));
-	// 	echo $new_id;
-	// 	$new_id++;
-	// }
-// 	$req = $bdd->query('SELECT id, secondaryID FROM note_bis');
-// 	while($data = $req->fetch())
-// 	{
-// 		echo "Pour id: ";
-// 		echo $data['id'];
-// 		echo "<br>on a secondaryID: ";
-// 		echo $data['secondaryID']."<br><br>";
-// 	}
-// 	$req = $bdd->query('SELECT id, secondaryID FROM note_bis');
-// 	$new_id=0;
-// 	$old_id=0;
-// 	while($data = $req->fetch())
-// 	{
-// 		$bdd->exec('UPDATE note_bis SET secondaryID = $new_id');
-// 		$new_id++;
-// 	}
-
-// 	$req =$bdd->query('SELECT id, secondaryID FROM note_bis');
-// 	echo "<br><br>Résultat des courses<br><br>";
-// 	while($data = $req->fetch())
-// 	{
-// 		echo "Pour id: ";
-// 		echo $data['id'];
-// 		echo "<br>on a secondaryID: ";
-// 		echo $data['secondaryID']."<br><br>";
-// 	}
-// ?>
-<?php
 session_start();
 try
 {
@@ -72,17 +9,40 @@ catch(Exeption $e)
 	die('Erreur : ' . $e->getMessage());
 }
 $new_value=10;
-$name=$_SESSION['name'];
-$bdd->query("UPDATE note_bis SET secondaryID=$new_value WHERE pseudo='$name'");
+$new_id=0;
 
-$result = $bdd->query("SELECT COUNT(*) as nbr_comments FROM note WHERE pseudo='$name'");
+$result = $bdd->query("SELECT COUNT(*) as nbr_comments FROM note_bis");
 $data = $result->fetch();
-$nbr_coms = htmlspecialchars($data['nbr_comments']);
-print "$nbr_coms";
+$nbr_msg=$data['nbr_comments'];
+echo "Il y a ".$nbr_msg." à traiter pour cette requête.<br><br>";
+
+$i=1;
+$i_req=1;
+$i_separator=0;
+
+while($i <= $nbr_msg)
+{
+	if($i_separator == 0)
+	{
+		echo "---<br>";
+		$i_separator = 2;
+	}
+	$req_recup_msg = $bdd->query("SELECT * FROM note_bis WHERE id='$i_req'");
+	$data1 = $req_recup_msg->fetch();
+	$msg = $data1['secondaryID'];
+	if($msg)
+	{
+		echo "Rang: ".$i." -> msg: ".$data1['commentaire']."<br>";
+		$bdd->query("UPDATE note_bis SET secondaryID='$i' WHERE id='$i_req'");
+		$i_separator--;
+	}
+	else
+	{
+		$i--;
+	}
+	$i_req++;
+	$i++;
+}
+
+echo '<a href="http://192.168.0.50/note.php?page=1" > Redirection </a>';
 ?>
-<!DOCTYPE html>
-<html>
-<body>
-	<a href="http://192.168.0.50/note.php?page=1">Cliquer ici pour la redirection</a>
-</body>
-</html>
